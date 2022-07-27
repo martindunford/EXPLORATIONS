c3c3d162704aedb5c882f1ac31a65e83159054bc
Martin Dunford <martin.dunford@corehr.com>
2022-07-27 13:22:04 +0100

    Added notes re why some of our PlSql code will not parse (tokens from parser used as column names in our tables I think!!!)

--- plsql_to_sqlalchemy.py
+++ plsql_to_sqlalchemy.py
@@ -8,16 +8,19 @@ import astunparse
 import sys
 
 from contextlib import *
+# Insert into table1 (TEMPLATE_ID,POS,COLUMN_VALUE,COLUMN_HEADER) values ('2020040717017321',1,'PAY_CODE','Pay Code');
+#  FAILS to parse because - I suspect - of the "column_value" Identifier i.e  rename
+#   identifier to "column_valu" below and all well !!
+#
+# NOTE: Very likely related to tokens in the parser i.e
+#   antlr_plsql/antlr_py/plsqlLexer.tokens
+#         74:COLUMN_VALUE=74
+# NOTE1: "position" is also a reserved token so column name matching same
+#          will also not parse
 
-plsql1 = '''
-        update ct_advanced_option  set enabled = 'Y'where company = p_company and option_name = 'LEAVE REQUEST PROCESSES';
-        Insert into ct_advanced_option (COMPANY,OPTION_NAME,ENABLED) values (p_company,'LEAVE REQUEST PROCESSES','Y');
+will_not_parse = '''
+        insert into table3 (column_value) values ('1');
 '''
-
-plsql1 = '''
-        update ct_advanced_option  set enabled = 'Y',martin = 100 ;
-        Insert into ct_advanced_option (COMPANY,OPTION_NAME,ENABLED) values (p_company,'LEAVE REQUEST PROCESSES','Y');
-        '''
 
 def main():
     lines = open('/Users/martin/CoreHR_Projects/roster/Roster_Test_Suite/CONFIGS/cost_roster_data_setup.bod').read()
@@ -26,12 +29,12 @@ def main():
 
     # Generates huge lines of error output in parsing..just want to see what line is not parsing !!
     #  so use the contextlib to send stderr to help.txt file !!
+
     os.system ('rm -f stderr.txt')
     with open('stderr.txt', 'w') as f:
         with redirect_stderr(f):
             t1 = ast.parse(lines)
-
-
+            # t1 = ast.parse(will_not_parse)
 
     # See: https://github.com/datacamp/antlr-plsql/blob/master/antlr_plsql/ast.py
     for node in walk(t1):
@@ -54,8 +57,5 @@ def main():
                     logger.info (f'{tick}{type(expr1.left)}{type(expr1.op)}{type(expr1.right)}')
                 else:
                     logger.info (f'{cross_mark} {type(upd.expression)}')
-
-
-
 if __name__ == '__main__':
     main()
