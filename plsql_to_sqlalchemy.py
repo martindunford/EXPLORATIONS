3cac51b70651efdc8e9e474f9dbc0a7831eec1e5
Martin Dunford <martin.dunford@corehr.com>
2022-07-28 12:12:02 +0100

    Added code to parse Plsql procedure and Select statements and output SqlAlchemy Python code for same

--- plsql_to_sqlalchemy.py
+++ plsql_to_sqlalchemy.py
@@ -22,8 +22,32 @@ will_not_parse = '''
         insert into table3 (column_value) values ('1');
 '''
 
+pl1 = '''
+procedure setGlobals(p_company in test_data_pay_group.orgz_ref%type,
+                     p_manager in varchar2) is 
+
+begin
+	select company_prefix1,company_prefix2
+	into g_prefix_1, g_prefix_2
+	from test_data_pay_group
+	where orgz_ref  = p_company;
+
+	select p_manager,
+		   forename,
+		   surname
+	into
+	g_manager,
+	g_manager_forname,
+	g_manager_surname
+	from hr_person
+	where personnel_no = p_manager;
+
+end setGlobals;
+
+'''
 def main():
-    lines = open('/Users/martin/CoreHR_Projects/roster/Roster_Test_Suite/CONFIGS/cost_roster_data_setup.bod').read()
+    # lines = open('/Users/martin/CoreHR_Projects/roster/Roster_Test_Suite/CONFIGS/cost_roster_data_setup.bod').read()
+    lines = open('/Users/martin/CoreHR_Projects/roster/Roster_Test_Suite/CONFIGS/timesheet_config.bod').read()
 
     # t1 = ast.parse(plsql1)
 
@@ -33,7 +57,8 @@ def main():
     os.system ('rm -f stderr.txt')
     with open('stderr.txt', 'w') as f:
         with redirect_stderr(f):
-            t1 = ast.parse(lines)
+            # t1 = ast.parse(lines)
+            t1 = ast.parse(pl1)
             # t1 = ast.parse(will_not_parse)
 
     # See: https://github.com/datacamp/antlr-plsql/blob/master/antlr_plsql/ast.py
@@ -57,5 +82,48 @@ def main():
                     logger.info (f'{tick}{type(expr1.left)}{type(expr1.op)}{type(expr1.right)}')
                 else:
                     logger.info (f'{cross_mark} {type(upd.expression)}')
+
+        elif t1 == ast.SelectStmt:
+            for clause in [node.target_list, node.from_clause, node.into_clause]:
+                logger.info (f'{tick}{type(clause)}')
+                for item in clause:
+                    logger.info (item.get_text())
+            table_name  = f'{node.from_clause[0].get_text()}'
+            select_stmt = f'res = self.find({table_name}).filter(F1).one()'
+
+            c1 = node.where_clause
+            left = c1.left.get_text()
+            right = c1.right.get_text()
+            op = c1.op.get_text()
+            if op == '=':
+                op = '=='
+            filter = f'F1 = {table_name}.{left} {op} {right}'
+            logger.info (f'{sun} {filter}')
+            logger.info (f'{sun} {select_stmt}')
+
+            assigns = []
+            for x,y in zip(node.target_list,node.into_clause):
+                cname = x.get_text()
+                varname = y.get_text()
+                assigns.append(f'{varname} = res.{cname}')
+            for astmt in assigns:
+                logger.info (f'{sun} {astmt}')
+
+
+
+        # doesn't recognize ast.Create_procedure_body as a class type to be compared to
+        # (Dynamic class?) , so match the string instead
+        elif 'Create_procedure_body' in str(t1):
+            pname = node.procedure_name.get_text()
+            plist = []
+            for item in node.parameter:
+                plist.append (item.parameter_name.get_text())
+                # logger.info (item.type_spec.get_text())
+            # Generate Python method definition
+            logger.info (f'{sun} def {pname}({",".join(plist)}):')
+        else:
+            logger.info (t1)
+            # if t1 == ast.Create_procedure_body:
+            #     logger.info (f'{sun}{sun}{sun}{sun}{sun}{sun}')
 if __name__ == '__main__':
     main()
